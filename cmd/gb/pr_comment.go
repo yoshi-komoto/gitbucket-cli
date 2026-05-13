ba6b8246fb2ef5ceb29e6b75bb38ba2dfd543c88
甲元 祥朗 <yoshi-komoto@i-mobilegroup.com>
2026-05-13 14:18:00 +0900

    feat(cmd): add 'pr comment list' subcommand

---
+++ cmd/gb/pr_comment.go
@@ -0,0 +1,12 @@
+package gb
+
+import "github.com/spf13/cobra"
+
+var prCommentCmd = &cobra.Command{
+	Use:   "comment",
+	Short: "Manage pull request comments",
+}
+
+func init() {
+	prCmd.AddCommand(prCommentCmd)
+}
---
+++ cmd/gb/pr_comment_list.go
@@ -0,0 +1,35 @@
+package gb
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+
+	"github.com/spf13/cobra"
+	"github.com/yoshi-komoto/gitbucket-cli/internal/output"
+)
+
+var prCommentListCmd = &cobra.Command{
+	Use:   "list <number>",
+	Short: "List comments on a pull request",
+	Args:  cobra.ExactArgs(1),
+	RunE: func(cmd *cobra.Command, args []string) error {
+		n, err := strconv.Atoi(args[0])
+		if err != nil || n <= 0 {
+			return fmt.Errorf("number must be a positive integer")
+		}
+		s, err := newSession(cmd.Context(), flags.repo)
+		if err != nil {
+			return err
+		}
+		cs, err := s.client.ListIssueComments(cmd.Context(), s.owner, s.repo, n)
+		if err != nil {
+			return err
+		}
+		return output.RenderCommentList(os.Stdout, flags.output, cs)
+	},
+}
+
+func init() {
+	prCommentCmd.AddCommand(prCommentListCmd)
+}
